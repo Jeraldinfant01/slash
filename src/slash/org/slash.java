afc341c01d509265cafe2f5fcf7bc7921b2fb7ea
JeraldInfant01 <jeraldinfant01@gmail.com>
2023-05-18 12:46:42 +0530

    codepushed

---
+++ src/slash/org/slash.java
@@ -0,0 +1,24 @@
+package slash.org;
+
+public class slash {
+
+	private void student1() {
+		System.out.println("the name is antony");
+
+	}
+	private void students2() {
+		System.out.println("the name is jerald");
+
+	}
+	
+	public static void main(String[] args) {
+		slash s = new slash();
+		s.student1();
+		s.students2();
+		
+		
+		
+	}
+	
+	
+}
